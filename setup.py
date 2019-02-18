0841886a75822f37c997165bd38a0814c796005b
Steven Blake <s.blake.08@aberdeen.ac.uk>
2019-02-18 22:31:01 +0000

    More setup.py config

--- setup.py
+++ setup.py
@@ -2,8 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='hsp',
-    version='1.0.0',
+    version='0.1.0',
     packages=find_packages(),
     license='MIT',
-    zip_safe=False
+    zip_safe=False,
+    install_requires=[
+        'requests >= 2.0'
+    ],
+    tests_require=[
+        'responses ~= 0.10.5'
+    ]
 )
